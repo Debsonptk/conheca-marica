d2be149770599b6dc5a0d7360f9533b0aab5b890
DebsonCoelho <debsoncoelh@gmail.com>
2023-09-13 19:40:36 -0300

    Create api, files and assets

--- src/Config.ts
+++ src/Config.ts
@@ -3,6 +3,12 @@ const Config = {
     name: import.meta.env.VITE_APP_NAME,
     version: import.meta.env.PACKAGE_VERSION,
   },
+
+  api: {
+    baseUrl: import.meta.env.VITE_MARICA_BASE_URL,
+    token: import.meta.env.VITE_API_TOKEN,
+  },
+
   i18n: {
     debbug: JSON.parse(
       (import.meta.env.VITE_I18N_DEBBUG ?? 'false').toLocaleLowerCase(),
---
+++ src/styles/GlobalStyles/index.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+    :root{
+    };
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    };
+
+    html, body, #root{
+        min-height: 100vh;
+    };
+
+    p {
+        margin-top: 0;
+        margin-bottom: 0;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: inherit;
+    }
+
+`
